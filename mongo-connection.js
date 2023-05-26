6af333932c88b5bc668b51ad224e80102ac82b32
Thamires Pereira Maciel <thamiresspm15@gmail.com>
2023-05-26 20:39:11 -0300

    Delete mongo-connection.js

--- mongo-connection.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient, ServerApiVersion }  from 'mongodb';
-const uri = "mongodb+srv://thamires2022:thamirespm50@cluster0.wsel3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-export const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
